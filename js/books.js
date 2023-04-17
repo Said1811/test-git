efa685fd40069958bba565b14eac5a74c1537784
Said1811 <127858387+Said1811@users.noreply.github.com>
2023-04-17 10:05:21 +0500

    Add files via upload
    First site

---
+++ js/books.js
@@ -0,0 +1,395 @@
+let books = [
+    {
+      "author": "Pushkin kochasi 7",
+      "country": "Nigeria",
+      "imageLink": "",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+      "pages": "281$",
+      "title": "Mirzo-Ulug'bek",
+      "year": 2002
+    },
+    
+    {
+      "author": "SH.Rashidov 63",
+      "country": "Denmark",
+      "imageLink": "images/fairy-tales.jpg",
+      "language": "Danish",
+      "link": "https://www.olx.uz/\n",    
+        "pages": "610$",
+      "title": "Yunusobod",
+      "year": 2007
+    },
+    {
+      "author": "Bog'ishamol 1",
+      "country": "Italy",
+      "imageLink": "images/the-divine-comedy.jpg",
+      "language": "Italian",
+      "link": "https://www.olx.uz/\n",   
+         "pages": "223$",
+      "title": "Shayxontoxur",
+      "year": 2005
+    },
+    {
+      "author": "Yarashev 23",
+      "country": "Sumer and Akkadian Empire",
+      "imageLink": "download.jpeg",
+      "language": "Akkadian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "291$",
+      "title": "Shayxontoxur",
+      "year": 1968
+    },
+    {
+      "author": "Labzak 3",
+      "country": "Achaemenid Empire",
+      "imageLink": "images/the-book-of-job.jpg",
+      "language": "Hebrew",
+      "link": "https://www.olx.uz/\n", 
+           "pages": "506$",
+      "title": "Uchtepa",
+      "year": 2003
+    },
+    {
+      "author": "Buyuk Ipak Yo'li",
+      "country": "India/Iran/Iraq/Egypt/Tajikistan",
+      "imageLink": "images/one-thousand-and-one-nights.jpg",
+      "language": "Arabic",
+      "link": "https://www.olx.uz/\n", 
+           "pages": "468$",
+      "title": "Yunusobod",
+      "year": 2012
+    },
+    {
+      "author": "Mirabad",
+      "country": "Iceland",
+      "imageLink": "images/njals-saga.jpg",
+      "language": "Old Norse",
+      "link": "https://www.olx.uz/\n", 
+           "pages": "721$",
+      "title": "Uzbekistan hotel",
+      "year": 1998
+    },
+    // suiiiiiiiiiiiiiiiiii
+    {
+      "author": "Amir Timur",
+      "country": "United Kingdom",
+      "imageLink": "images/pride-and-prejudice.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n", 
+           "pages": "168$",
+      "title": "Yashnobod",
+      "year": 1977
+    },
+    {
+      "author": "Buyuk Turon",
+      "country": "France",
+      "imageLink": "images/le-pere-goriot.jpg",
+      "language": "French",
+      "link": "https://www.olx.uz/\n",
+            "pages": "538$",
+      "title": "Yunusobod",
+      "year": 1981 
+    },
+    {
+      "author": "Navoiy ko'chasi",
+      "country": "Republic of Ireland",
+      "imageLink": "images/molloy-malone-dies-the-unnamable.jpg",
+      "language": "French, English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "256$",
+      "title": "Shayxontoxur",
+      "year": 1960
+    },
+    {
+      "author": "Yunusobod-8",
+      "country": "Argentina",
+      "imageLink": "images/ficciones.jpg",
+      "language": "Spanish",
+      "link": "https://www.olx.uz/\n",
+            "pages": "716$",
+      "title": "Chilonzor",
+      "year": 1983
+    },
+    {
+      "author": "SH.Rashidov,1",
+      "country": "United Kingdom",
+      "imageLink": "images/wuthering-heights.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "666$",
+      "title": "Yakkasaroy",
+      "year": 1992
+    },
+    {
+      "author": "Istiqbol ko'chasi",
+      "country": "Algeria, French Empire",
+      "imageLink": "images/l-etranger.jpg",
+      "language": "French",
+      "link": "https://www.olx.uz/\n",
+            "pages": "120$",
+      "title": "Olmazor",
+      "year": 1988
+    },
+    {
+      "author": "Pahlavon Mahmud",
+      "country": "Romania, France",
+      "imageLink": "images/poems-paul-celan.jpg",
+      "language": "German",
+      "link": "https://www.olx.uz/\n",
+      "pages": "914$",
+      "title": "Chilonzor",
+      "year": 1985
+    },
+    {
+      "author": "Shota Rustaveli",
+      "country": "France",
+      "imageLink": "images/voyage-au-bout-de-la-nuit.jpg",
+      "language": "French",
+      "link": "https://www.olx.uz/\n",
+            "pages": "276$",
+      "title": "Mirobod",
+      "year": 2001
+    },
+    {
+      "author": "Amir Temur xiyoboni",
+      "country": "Spain",
+      "imageLink": "images/don-quijote-de-la-mancha.jpg",
+      "language": "Spanish",
+      "link": "https://www.olx.uz/\n",
+            "pages": "377$",
+      "title": "Sergeli",
+      "year": 2017
+    },
+    {
+      "author": "Chortoq ko'chasi",
+      "country": "England",
+      "imageLink": "images/the-canterbury-tales.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "632$",
+      "title": "Shayxontoxur",
+      "year": 1992
+    },
+    {
+      "author": "Chilanzor hudud",
+      "country": "Russia",
+      "imageLink": "images/stories-of-anton-chekhov.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "526$",
+      "title": "Yunusobod",
+      "year": 1982
+    },
+    {
+      "author": "Buxoro ko'chasi",
+      "country": "United Kingdom",
+      "imageLink": "images/nostromo.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "543$",
+      "title": "Obnazir",
+      "year": 2019
+    },
+    {
+      "author": "Marg'iloniy",
+      "country": "United Kingdom",
+      "imageLink": "images/great-expectations.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "362$",
+      "title": "Yashnobod",
+      "year": 2007
+    },
+    {
+      "author": "Beruniy ko'chasi 2/6",
+      "country": "France",
+      "imageLink": "images/jacques-the-fatalist.jpg",
+      "language": "French",
+      "link": "https://www.olx.uz/\n",
+            "pages": "596$",
+      "title": "Sergeli",
+      "year": 1981
+    },
+    {
+      "author": "Bobur ko'chasi 3",
+      "country": "Germany",
+      "imageLink": "images/berlin-alexanderplatz.jpg",
+      "language": "German",
+      "link": "https://www.olx.uz/\n",
+            "pages": "600$",
+      "title": "Chilonzor",
+      "year": 1991
+    },
+    {
+      "author": "O'zbekiston ko'chasi 221",
+      "country": "Russia",
+      "imageLink": "images/crime-and-punishment.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "551$",
+      "title": "Olmazor",
+      "year": 2010
+    },
+    {
+      "author": "Niyozbek yo'li 1",
+      "country": "Russia",
+      "imageLink": "images/the-idiot.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "656$",
+      "title": "Uchtepa",
+      "year": 2020
+    },
+    {
+      "author": "Bostanliq ko'chasi",
+      "country": "Russia",
+      "imageLink": "images/the-possessed.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "768$",
+      "title": "Olmazor",
+      "year": 1999
+    },
+    {
+      "author": "Xalqlar do'stligi 44a",
+      "country": "Russia",
+      "imageLink": "images/the-brothers-karamazov.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n", 
+           "pages": "824$",
+      "title": "The Brothers Karamazov",
+      "year": 2010
+    },
+    {
+      "author": "Navoiy xiyoboni",
+      "country": "United Kingdom",
+      "imageLink": "images/middlemarch.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "568$",
+      "title": "Yunusobod",
+      "year": 2003
+    },
+    {
+      "author": "Foziltepa ko'chasi",
+      "country": "United States",
+      "imageLink": "images/invisible-man.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "581$",
+      "title": "Yakkasaroy",
+      "year": 1985
+    },
+    {
+      "author": "A.Chekhov ko'chasi",
+      "country": "Greece",
+      "imageLink": "images/medea.jpg",
+      "language": "Greek",
+      "link": "https://www.olx.uz/\n",
+            "pages": "948$",
+      "title": "Shayxontoxur",
+      "year": 1980
+    },
+    {
+      "author": "Farobiy ko'chasi",
+      "country": "United States",
+      "imageLink": "images/absalom-absalom.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "1000$",
+      "title": "Mirzo-Ulug'bek",
+      "year": 2008
+    },
+    {
+      "author": "Ipakchilik 58",
+      "country": "United States",
+      "imageLink": "images/the-sound-and-the-fury.jpg",
+      "language": "English",
+      "link": "https://www.olx.uz/\n",
+            "pages": "326$",
+      "title": "Mirzo-Ulug'bek",
+      "year": 1986
+    },
+    {
+      "author": "Guliston 59-60",
+      "country": "France",
+      "imageLink": "images/madame-bovary.jpg",
+      "language": "French",
+      "link": "https://www.olx.uz/\n",
+            "pages": "528$",
+      "title": "Mirobod",
+      "year": 2004
+    },
+    {
+      "author": "Gagarin ko'chasi 40",
+      "country": "France",
+      "imageLink": "images/l-education-sentimentale.jpg",
+      "language": "French",
+      "link": "https://www.olx.uz/\n", 
+           "pages": "606$",
+      "title": "Yakkasaroy",
+      "year": 1989
+    },
+    {
+      "author": "Furqat ko'chasi 2",
+      "country": "Spain",
+      "imageLink": "images/gypsy-ballads.jpg",
+      "language": "Spanish",
+      "link": "https://www.olx.uz/\n",
+            "pages": "218$",
+      "title": "Chilonzor",
+      "year": 1995
+    },
+    {
+      "author": "Xamid Olimjon",
+      "country": "Colombia",
+      "imageLink": "images/one-hundred-years-of-solitude.jpg",
+      "language": "Spanish",
+      "link": "https://www.olx.uz/\n",
+            "pages": "417$",
+      "title": "Yunusobod",
+      "year": 1982
+    },
+    {
+      "author": "Bog'zor ko'chasi 22",
+      "country": "Colombia",
+      "imageLink": "images/love-in-the-time-of-cholera.jpg",
+      "language": "Spanish",
+      "link": "https://www.olx.uz/\n",
+            "pages": "368$",
+      "title": "Yashnobod",
+      "year": 2021
+    },
+    {
+      "author": "Turon ko'chasi 23",
+      "country": "Saxe-Weimar",
+      "imageLink": "images/faust.jpg",
+      "language": "German",
+      "link": "https://www.olx.uz/\n",
+            "pages": "158$",
+      "title": "Sergeli",
+      "year": 2015
+    },
+    {
+      "author": "Kichik xalqa yoli",
+      "country": "Russia",
+      "imageLink": "images/dead-souls.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "432$",
+      "title": "Yashnobod",
+      "year": 2013
+    },
+    {
+      "author": "Ulug'bek ko'chasi 32",
+      "country": "Russia",
+      "imageLink": "images/dead-souls.jpg",
+      "language": "Russian",
+      "link": "https://www.olx.uz/\n",
+            "pages": "948$",
+      "title": "Olmazor",
+      "year": 2006
+    },
+    
+  ]
